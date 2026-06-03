ecbd5356873cc3c8406234b47b8ff9ee73e75cd2
patriceckhart <mail@patriceckhart.com>
2026-06-03 19:07:57 +0200

    Initial Commit

---
+++ zot sidekick/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  zot sidekick
+//
+//  Created by Patric Eckhart on 03.06.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ zot sidekick/zot_sidekickApp.swift
@@ -0,0 +1,17 @@
+//
+//  zot_sidekickApp.swift
+//  zot sidekick
+//
+//  Created by Patric Eckhart on 03.06.26.
+//
+
+import SwiftUI
+
+@main
+struct zot_sidekickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
